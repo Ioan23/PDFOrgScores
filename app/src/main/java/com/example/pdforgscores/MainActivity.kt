11ffc607ecd881040249773832efeef67ae6f929
Ioan23 <75176548+Ioan23@users.noreply.github.com>
2023-09-05 18:29:57 +0300

    saving-user-to-firebase

--- app/src/main/java/com/example/pdforgscores/MainActivity.kt
+++ app/src/main/java/com/example/pdforgscores/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.pdforgscores
 
+import android.content.ContentValues.TAG
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -11,17 +13,36 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.pdforgscores.ui.theme.PDFORGScoresTheme
+import com.google.firebase.firestore.FirebaseFirestore
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             PDFORGScoresTheme {
+
+                val db = FirebaseFirestore.getInstance()
+                val user: MutableMap<String, Any> = HashMap()
+
+                user["firstName"] = "John"
+                user["lastName"] = "Smith"
+
+
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
+                    db.collection("users")
+                        .add(user)
+                        .addOnSuccessListener {
+                            Log.d( "FB", "onCreate: ${it.id}")
+
+                        }.addOnFailureListener {
+                            Log.d( "FB", "onCreate: $it")
+
+                        }
+
                     Greeting("Android")
                 }
             }
